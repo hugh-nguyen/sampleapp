6a9bd94f7f82134cd5f931462ec5bcfbbd590c0f
Hugh <huy.nguyen@live.com>
2023-06-10 23:25:17 +1000

    terraform

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  security_groups = [aws_security_group.web_sg.id]
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
     Name = "web-app-instance"
