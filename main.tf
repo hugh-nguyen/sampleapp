5924b64f4a3e5ad7fe8f880f5f6ffc5c3b05f10b
Hugh <huy.nguyen@live.com>
2023-06-10 23:20:28 +1000

    terraform

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "web_instance" {
+resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   security_groups = [aws_security_group.web_sg.id]
