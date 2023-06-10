23d881b2224a0463933f5469161177ce4a98679f
Hugh <huy.nguyen@live.com>
2023-06-10 23:18:54 +1000

    terraform

--- main.tf
+++ main.tf
@@ -38,8 +38,24 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "web_instance" {
-  ami           = "ami-0c94855ba95c71c99"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   security_groups = [aws_security_group.web_sg.id]
 
